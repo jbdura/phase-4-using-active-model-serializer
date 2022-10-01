e391d07dab2ae4d9fe3d519c45f055dcd73f634b
jbdura <bidurajohnson@gmail.com>
2022-10-01 18:45:01 +0300

    Done

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,8 +8,24 @@ class MoviesController < ApplicationController
 
   def show
     movie = Movie.find(params[:id])
+    # render json: movie.to_json(only: 
+    #   [:id, :title, :year, :length, :director, :description, :poster_url, :category, :discount, :female_director])
+    # render json: movie.to_json(except: [:created_at, :updated_at])
+    # render json: post.to_json(only: [:title, :description, :id], include: [author: { only: [:name]}])
     render json: movie
   end
+
+  def summary
+    movie = Movie.find(params[:id])
+    render json: movie, serializer: MovieSummarySerializer
+  end
+
+  def summaries
+    movies = Movie.all
+    render json: movies, each_serializer: MovieSummarySerializer
+  end
+
+  
 
   private
 
